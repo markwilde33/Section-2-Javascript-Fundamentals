9d68d974617bd5af472b550865c05edce202048a
Mark Wilde <markwilde29@gmail.com>
2020-05-06 15:22:45 +0100

    Data types in Javascript.

--- sandbox.js
+++ sandbox.js
@@ -1,52 +1,26 @@
-// var, let, const
+// PRIMITIVE
 
-// VAR
-// var name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
+// String
+const name = "John Doe";
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
 
-// // Init var
-// var greeting;
-// console.log(greeting);
-// greeting = 'Hello';
-// console.log(greeting);
-
-// // letters, numbers, _, $
-// // Can not start with number
-
-// // Multi word vars
-// var firstName = 'John'; // Camel case
-// var first_name = 'Sara'; // Underscore
-// var FirstName = 'Tom'; // Pascal case
-// var firstname;
-
-// LET
-// let name;
-// name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
-
-// CONST
-// const name = 'John';
-// console.log(name);
-// Can not reassign
-// name = 'Sara';
-// Have to assign a value
-// const greeting;
-
-const person = {
-  name: "John",
-  age: 30,
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ["movies", "music"];
+// Object literal
+const address = {
+  city: "Boston",
+  state: "MA",
 };
-
-person.name = "Sara";
-person.age = 32;
-
-// console.log(person);
-
-const numbers = [1, 2, 3, 4, 5];
-numbers.push(6);
-
-console.log(numbers);
+const today = new Date();
+console.log(today);
+console.log(typeof today);
