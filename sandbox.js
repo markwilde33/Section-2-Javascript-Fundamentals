f43cd3dd0c8a78abaf039f9d4d2b7439c5a2cc85
Mark Wilde <markwilde29@gmail.com>
2020-05-06 15:53:02 +0100

    Numbers and the Math object.

--- sandbox.js
+++ sandbox.js
@@ -1,39 +1,27 @@
-let val;
+const num1 = 100;
+const num2 = 50;
 
-// Number to string
-val = String(555);
-val = String(4 + 4);
-// Bool to string
-val = String(true);
-// Date to string
-val = String(new Date());
-// Array to string
-val = String([1, 2, 3, 4]);
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
 
-// toString()
-val = (5).toString();
-val = true.toString();
+// Math object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(128);
+val = Math.abs(-5);
+val = Math.pow(16, 4);
+val = Math.min(7, 78, 89, 34, 6, -3);
+val = Math.max(7, 78, 89, 34, 6, -3);
+val = Math.random();
 
-// String to number
-val = Number("5");
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number("hello");
-val = Number([1, 2, 3]);
+// useful
+val = Math.floor(Math.random() * 20 + 1);
 
-val = parseInt("100.30");
-val = parseFloat("100.30");
-
-// Output
-// console.log(val);
-// console.log(typeof val);
-// //console.log(val.length);
-// console.log(val.toFixed(2));
-
-const val1 = String(5);
-const val2 = 6;
-const sum = Number(val1 + val2);
-
-console.log(sum);
-console.log(typeof sum);
+val = console.log(val);
