0313957c60b40a12e8f18a9aa183f0a4722f2437
Mark Wilde <markwilde29@gmail.com>
2020-05-07 19:21:25 +0100

    Function declarations and expressions.

--- sandbox.js
+++ sandbox.js
@@ -1,41 +1,47 @@
-const color = "yellow";
+// FUNCTION DECLARATIONS
 
-switch (color) {
-  case "red":
-    console.log("Color is red");
-    break;
-  case "blue":
-    console.log("Color is blue");
-    break;
-  default:
-    console.log("Color is not red or blue");
-    break;
+function greet(firstName = "John", lastName = "Doe") {
+  // if(typeof firstName === 'undefined'){firstName = 'John'}
+  // if(typeof lastName === 'undefined'){lastName = 'Doe'}
+  //console.log('Hello');
+  return "Hello " + firstName + " " + lastName;
 }
 
-let day;
+// console.log(greet());
 
-switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-    day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-    break;
-}
+// FUNCTION EXPRESIONS
 
-console.log(`Today is ${day}`);
+const square = function (x = 3) {
+  return x * x;
+};
+
+// console.log(square());
+
+// IMMEDIATELY INVOCABLE FUNCTION EXPRESSIONS - IIFEs
+
+// (function(){
+//   console.log('IIFE Ran..');
+// })();
+
+// (function(name){
+//   console.log('Hello '+ name);
+// })('Paul');
+
+// PROPERTY METHODS
+
+const todo = {
+  add: function () {
+    console.log("Add todo..");
+  },
+  edit: function (id) {
+    console.log(`Edit todo ${id}`);
+  },
+};
+
+todo.delete = function () {
+  console.log("Delete todo...");
+};
+
+todo.add();
+todo.edit(22);
+todo.delete();
